1b53471c96035be1a8cbac8283802d0788cd75d2
the1whodevs <gerfil18-dev@yahoo.com>
2020-11-24 19:59:17 +0200

    Working on Controller

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -20,77 +20,88 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    
-    private void Update()
+
+    private void FixedUpdate()
     {
-        var v = Input.GetKey(KeyCode.W) ? 1.0f : Input.GetKey(KeyCode.S) ? -1.0f : 0.0f; //Input.GetAxis("Vertical");
-        var h = Input.GetKey(KeyCode.D) ? 1.0f : Input.GetKey(KeyCode.A) ? -1.0f : 0.0f; //Input.GetAxis("Horizontal");
-        var j = Input.GetKey(KeyCode.Space); //Input.GetAxis("Jump") > 0.0f;
-        var s = Input.GetKey(KeyCode.LeftShift); //Input.GetAxis("Sprint") > 0.0f;
-        var c = Input.GetKey(KeyCode.LeftControl); //Input.GetAxis("Crouch") > 0.0f;
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
 
-        var moveDir = Vector3.zero;
-
-        if (v > 0.0f) moveDir += transform.forward;
-        else if (v < 0.0f) moveDir += -transform.forward;
+        var move = transform.forward * v +
+                   transform.right * h;
         
-        if (h > 0.0f) moveDir += transform.right;
-        else if (h < 0.0f) moveDir += -transform.right;
-
-        moveDir.Normalize();
-
-        var isMoving = !Mathf.Approximately(v, 0.0f) ||
-                       !Mathf.Approximately(h, 0.0f);
-        
-        if (j && canJump)
-        {
-            canJump = false;
-            CurrentMoveState = PlayerMoveState.Jumping;
-        }
-        else if (isMoving)
-        {
-            if (s) CurrentMoveState = PlayerMoveState.Sprint;
-            else CurrentMoveState = c ? PlayerMoveState.CrouchRun : PlayerMoveState.Run;
-        }
-        else
-        {
-            CurrentMoveState = PlayerMoveState.Idle;
-        }
-
-        switch (CurrentMoveState)
-        {
-            case PlayerMoveState.Idle:
-                Player.instance.Animator.Idle();
-                //rb.velocity = Vector3.zero;
-                return;
-            
-            case PlayerMoveState.CrouchIdle:
-                Player.instance.Animator.Idle();
-                //rb.velocity = Vector3.zero;
-                return;
-            
-            case PlayerMoveState.Run:
-                Player.instance.Animator.Run();
-                rb.position += moveDir * (runSpeed * Time.deltaTime);
-                return;
-            
-            case PlayerMoveState.CrouchRun:
-                Player.instance.Animator.Run();
-                rb.position += moveDir * (crouchSpeed * Time.deltaTime);
-                return;
-            
-            case PlayerMoveState.Sprint:
-                Player.instance.Animator.Sprint();
-                rb.position += moveDir * (sprintSpeed * Time.deltaTime);
-                return;
-            
-            case PlayerMoveState.Jumping:
-                Player.instance.Animator.Idle();
-                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-                return;
-            
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+        rb.position += move * (runSpeed * Time.fixedDeltaTime);
     }
+
+    // private void Update()
+    // {
+    //     var v = Input.GetKey(KeyCode.W) ? 1.0f : Input.GetKey(KeyCode.S) ? -1.0f : 0.0f; //Input.GetAxis("Vertical");
+    //     var h = Input.GetKey(KeyCode.D) ? 1.0f : Input.GetKey(KeyCode.A) ? -1.0f : 0.0f; //Input.GetAxis("Horizontal");
+    //     var j = Input.GetKey(KeyCode.Space); //Input.GetAxis("Jump") > 0.0f;
+    //     var s = Input.GetKey(KeyCode.LeftShift); //Input.GetAxis("Sprint") > 0.0f;
+    //     var c = Input.GetKey(KeyCode.LeftControl); //Input.GetAxis("Crouch") > 0.0f;
+    //
+    //     var moveDir = Vector3.zero;
+    //
+    //     if (v > 0.0f) moveDir += transform.forward;
+    //     else if (v < 0.0f) moveDir += -transform.forward;
+    //     
+    //     if (h > 0.0f) moveDir += transform.right;
+    //     else if (h < 0.0f) moveDir += -transform.right;
+    //
+    //     moveDir.Normalize();
+    //
+    //     var isMoving = !Mathf.Approximately(v, 0.0f) ||
+    //                    !Mathf.Approximately(h, 0.0f);
+    //     
+    //     if (j && canJump)
+    //     {
+    //         canJump = false;
+    //         CurrentMoveState = PlayerMoveState.Jumping;
+    //     }
+    //     else if (isMoving)
+    //     {
+    //         if (s) CurrentMoveState = PlayerMoveState.Sprint;
+    //         else CurrentMoveState = c ? PlayerMoveState.CrouchRun : PlayerMoveState.Run;
+    //     }
+    //     else
+    //     {
+    //         CurrentMoveState = PlayerMoveState.Idle;
+    //     }
+    //
+    //     switch (CurrentMoveState)
+    //     {
+    //         case PlayerMoveState.Idle:
+    //             Player.instance.Animator.Idle();
+    //             //rb.velocity = Vector3.zero;
+    //             return;
+    //         
+    //         case PlayerMoveState.CrouchIdle:
+    //             Player.instance.Animator.Idle();
+    //             //rb.velocity = Vector3.zero;
+    //             return;
+    //         
+    //         case PlayerMoveState.Run:
+    //             Player.instance.Animator.Run();
+    //             rb.position += moveDir * (runSpeed * Time.deltaTime);
+    //             return;
+    //         
+    //         case PlayerMoveState.CrouchRun:
+    //             Player.instance.Animator.Run();
+    //             rb.position += moveDir * (crouchSpeed * Time.deltaTime);
+    //             return;
+    //         
+    //         case PlayerMoveState.Sprint:
+    //             Player.instance.Animator.Sprint();
+    //             rb.position += moveDir * (sprintSpeed * Time.deltaTime);
+    //             return;
+    //         
+    //         case PlayerMoveState.Jumping:
+    //             Player.instance.Animator.Idle();
+    //             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+    //             return;
+    //         
+    //         default:
+    //             throw new ArgumentOutOfRangeException();
+    //     }
+    // }
 }
