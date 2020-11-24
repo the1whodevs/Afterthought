5ff58eb2cd5e29ba839cbfe0988f09f6c19be1fc
the1whodevs <gerfil18-dev@yahoo.com>
2020-11-24 20:19:13 +0200

    Input setup for controllers as well

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -21,92 +21,95 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    // private void FixedUpdate()
+    // {
+    //     var v = Input.GetAxis("Vertical");
+    //     var h = Input.GetAxis("Horizontal");
+    //
+    //     var move = transform.forward * v +
+    //                transform.right * h;
+    //
+    //     var m = Mathf.Abs(v) + Mathf.Abs(h);
+    //     if (m > 1.0f) m = 1.0f;
+    //
+    //     move.Normalize();
+    //     
+    //     rb.position += move * (m * runSpeed * Time.fixedDeltaTime);
+    // }
+
+    private void Update()
     {
         var v = Input.GetAxis("Vertical");
         var h = Input.GetAxis("Horizontal");
-
-        var move = transform.forward * v +
-                   transform.right * h;
-
+        var j = Input.GetAxisRaw("Jump") >= 1.0f;
+        var s = Input.GetAxisRaw("Sprint") >= 1.0f;
+        var c = Input.GetAxisRaw("Crouch") >= 1.0f;
+    
+        var moveDir = Vector3.zero;
+    
         var m = Mathf.Abs(v) + Mathf.Abs(h);
         if (m > 1.0f) m = 1.0f;
-
-        move.Normalize();
         
-        rb.position += move * (m * runSpeed * Time.fixedDeltaTime);
+        if (v > 0.0f) moveDir += transform.forward;
+        else if (v < 0.0f) moveDir += -transform.forward;
+        
+        if (h > 0.0f) moveDir += transform.right;
+        else if (h < 0.0f) moveDir += -transform.right;
+    
+        moveDir.Normalize();
+    
+        var isMoving = !Mathf.Approximately(v, 0.0f) ||
+                       !Mathf.Approximately(h, 0.0f);
+        
+        if (j && canJump)
+        {
+            canJump = false;
+            CurrentMoveState = PlayerMoveState.Jumping;
+        }
+        else if (isMoving)
+        {
+            if (s) CurrentMoveState = PlayerMoveState.Sprint;
+            else CurrentMoveState = c ? PlayerMoveState.CrouchRun : PlayerMoveState.Run;
+        }
+        else
+        {
+            CurrentMoveState = PlayerMoveState.Idle;
+        }
+    
+        switch (CurrentMoveState)
+        {
+            case PlayerMoveState.Idle:
+                Player.instance.Animator.Idle();
+                //rb.velocity = Vector3.zero;
+                return;
+            
+            case PlayerMoveState.CrouchIdle:
+                Player.instance.Animator.Idle();
+                //rb.velocity = Vector3.zero;
+                return;
+            
+            case PlayerMoveState.Run:
+                Player.instance.Animator.Run();
+                rb.position += moveDir * (m * runSpeed * Time.deltaTime);
+                return;
+            
+            case PlayerMoveState.CrouchRun:
+                Player.instance.Animator.Run();
+                rb.position += moveDir * (m * crouchSpeed * Time.deltaTime);
+                return;
+            
+            case PlayerMoveState.Sprint:
+                Player.instance.Animator.Sprint();
+                rb.position += moveDir * (m * sprintSpeed * Time.deltaTime);
+                return;
+            
+            case PlayerMoveState.Jumping:
+                Player.instance.Animator.Idle();
+                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                return;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
-
-    // private void Update()
-    // {
-    //     var v = Input.GetKey(KeyCode.W) ? 1.0f : Input.GetKey(KeyCode.S) ? -1.0f : 0.0f; //Input.GetAxis("Vertical");
-    //     var h = Input.GetKey(KeyCode.D) ? 1.0f : Input.GetKey(KeyCode.A) ? -1.0f : 0.0f; //Input.GetAxis("Horizontal");
-    //     var j = Input.GetKey(KeyCode.Space); //Input.GetAxis("Jump") > 0.0f;
-    //     var s = Input.GetKey(KeyCode.LeftShift); //Input.GetAxis("Sprint") > 0.0f;
-    //     var c = Input.GetKey(KeyCode.LeftControl); //Input.GetAxis("Crouch") > 0.0f;
-    //
-    //     var moveDir = Vector3.zero;
-    //
-    //     if (v > 0.0f) moveDir += transform.forward;
-    //     else if (v < 0.0f) moveDir += -transform.forward;
-    //     
-    //     if (h > 0.0f) moveDir += transform.right;
-    //     else if (h < 0.0f) moveDir += -transform.right;
-    //
-    //     moveDir.Normalize();
-    //
-    //     var isMoving = !Mathf.Approximately(v, 0.0f) ||
-    //                    !Mathf.Approximately(h, 0.0f);
-    //     
-    //     if (j && canJump)
-    //     {
-    //         canJump = false;
-    //         CurrentMoveState = PlayerMoveState.Jumping;
-    //     }
-    //     else if (isMoving)
-    //     {
-    //         if (s) CurrentMoveState = PlayerMoveState.Sprint;
-    //         else CurrentMoveState = c ? PlayerMoveState.CrouchRun : PlayerMoveState.Run;
-    //     }
-    //     else
-    //     {
-    //         CurrentMoveState = PlayerMoveState.Idle;
-    //     }
-    //
-    //     switch (CurrentMoveState)
-    //     {
-    //         case PlayerMoveState.Idle:
-    //             Player.instance.Animator.Idle();
-    //             //rb.velocity = Vector3.zero;
-    //             return;
-    //         
-    //         case PlayerMoveState.CrouchIdle:
-    //             Player.instance.Animator.Idle();
-    //             //rb.velocity = Vector3.zero;
-    //             return;
-    //         
-    //         case PlayerMoveState.Run:
-    //             Player.instance.Animator.Run();
-    //             rb.position += moveDir * (runSpeed * Time.deltaTime);
-    //             return;
-    //         
-    //         case PlayerMoveState.CrouchRun:
-    //             Player.instance.Animator.Run();
-    //             rb.position += moveDir * (crouchSpeed * Time.deltaTime);
-    //             return;
-    //         
-    //         case PlayerMoveState.Sprint:
-    //             Player.instance.Animator.Sprint();
-    //             rb.position += moveDir * (sprintSpeed * Time.deltaTime);
-    //             return;
-    //         
-    //         case PlayerMoveState.Jumping:
-    //             Player.instance.Animator.Idle();
-    //             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-    //             return;
-    //         
-    //         default:
-    //             throw new ArgumentOutOfRangeException();
-    //     }
-    // }
 }
