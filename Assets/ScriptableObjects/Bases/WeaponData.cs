fd13530fa5f5da378e8c632446c4cfd760b54bbd
Sneaky_Vaseline <13-12698@saeinsitute.edu>
2020-12-16 18:49:40 +0200

    Weapon Data + Weapon Icons

--- Assets/ScriptableObjects/Bases/WeaponData.cs
+++ Assets/ScriptableObjects/Bases/WeaponData.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 [CreateAssetMenu(menuName = "Veejay/Weapon", fileName = "Weapon")]
 public class WeaponData : ScriptableObject
 {
-    // TODO: Weapon image, name, description.
+    public enum WeaponType {Firearm, Projectile, Melee}
+    public enum FireType {FullAuto, Burst, SemiAuto, BoltAction}
+
+    [FormerlySerializedAs("rightHandPrefab")]
+
+    public new string name;
+    public string description;
+    public Sprite image;
     
-    [FormerlySerializedAs("rightHandPrefab")] 
-    [Header("This is the main prefab to be used, and should never be null.")]
-    public GameObject wepPrefab; 
+    public GameObject wepPrefab;
+    public GameObject projectilePrefab;
+    public GameObject muzzleEffect;
+    public GameObject bulletHole;
+
+    public WeaponType weaponType = WeaponType.Firearm;
+    public FireType fireType = FireType.FullAuto;
+    public int magazineCapacity = 30;
+    public float weaponDamage = 0.0f;
+    public float fireRate = 3.0f;
+    public float reloadSpeed = 1.0f;
+    public float mobilityPenalty = 0.2f;
+    public float adsMultiplier = 1.0f;
+
 }
