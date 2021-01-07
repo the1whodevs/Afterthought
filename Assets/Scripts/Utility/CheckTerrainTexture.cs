08a14338cc739d77b35abe1b26e98b7d8e76ead1
Sneaky_Vaseline <13-12698@saeinsitute.edu>
2021-01-07 23:15:11 +0200

    Added Footsteps Logic (sounds perfection to be done)

---
+++ Assets/Scripts/Utility/CheckTerrainTexture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTerrainTexture : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Terrain t;
+
+    private int posX;
+    private int posZ;
+    public float[] textureValues;
+
+    void Start () 
+    {
+        t = Terrain.activeTerrain;
+        playerTransform = gameObject.transform;
+    }
+    
+    public void GetTerrainTexture()
+    {
+        ConvertPosition(playerTransform.position);
+        CheckTexture();
+    }
+
+    void ConvertPosition(Vector3 playerPosition)
+    {
+        Vector3 terrainPosition = playerPosition - t.transform.position;
+
+        Vector3 mapPosition = new Vector3
+        (terrainPosition.x / t.terrainData.size.x, 0,
+            terrainPosition.z / t.terrainData.size.z);
+
+        float xCoord = mapPosition.x * t.terrainData.alphamapWidth;
+        float zCoord = mapPosition.z * t.terrainData.alphamapHeight;
+
+        posX = (int)xCoord;
+        posZ = (int)zCoord;
+    }
+
+    void CheckTexture()
+    {
+        float[,,] aMap = t.terrainData.GetAlphamaps (posX, posZ, 1, 1);
+        textureValues[0] = aMap[0,0,0];
+        textureValues[1] = aMap[0,0,1];
+        textureValues[2] = aMap[0,0,2];
+        textureValues[3] = aMap[0,0,3];
+    }
+}
