08b95b95e5d3192df2cbeed71d28a9acbbd18c3d
Gerasimos Filippakos <gerfil18-dev@yahoo.com>
2021-01-17 16:41:42 +0200

    Game Analytics

---
+++ Assets/Scripts/Utility/GAInit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GAInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameAnalyticsSDK.GameAnalytics.Initialize();
+        SceneManager.LoadScene(1);
+    }
+}
