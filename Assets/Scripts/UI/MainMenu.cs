2252567d6936ecdc55c7d3d65667092ad8281b77
Gerasimos Filippakos <gerfil18-dev@yahoo.com>
2021-01-19 00:22:38 +0200

    Main Menu + Settings Window almost ready

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,6 +12,17 @@ public class MainMenu : MonoBehaviour
     // The settings window.
     [SerializeField] private GameObject settingsPanel;
 
+    // The panels within the settings window.
+    [Header("Controls Panel")]
+    [SerializeField] private GameObject controlsPanel;
+    [SerializeField] private Slider horizontalSensitivitySlider;
+    [SerializeField] private TMP_InputField horizontalSensitivityIF;
+    [SerializeField] private Slider verticalSensitivitySlider;
+    [SerializeField] private TMP_InputField verticalSensitivityIF;
+
+    [SerializeField] private GameObject audioPanel;
+    [SerializeField] private GameObject graphicsPanel;
+
     public const string SAVE_FILE_PREFIX = "AFTERTHOUGHT_SAVE_";
     public const string HORIZONTAL_SENS_KEY = "MOUSE_X_SENSITIVITY";
     public const string VERTICAL_SENS_KEY = "MOUSE_Y_SENSITIVITY";
@@ -19,6 +32,20 @@ public class MainMenu : MonoBehaviour
         continueButton.SetActive(PlayerPrefs.HasKey($"{SAVE_FILE_PREFIX}0"));
         mainPanel.SetActive(true);
         settingsPanel.SetActive(false);
+
+        controlsPanel.SetActive(true);
+        audioPanel.SetActive(false);
+        graphicsPanel.SetActive(false);
+
+        var mouseX = PlayerPrefs.GetFloat(HORIZONTAL_SENS_KEY, 1.0f);
+
+        horizontalSensitivityIF.SetTextWithoutNotify((10 *mouseX).ToString("F0"));
+        horizontalSensitivitySlider.SetValueWithoutNotify(10.0f * mouseX);
+
+        var mouseY = PlayerPrefs.GetFloat(VERTICAL_SENS_KEY, 1.0f);
+
+        verticalSensitivityIF.SetTextWithoutNotify((10 * mouseY).ToString("F0"));
+        verticalSensitivitySlider.SetValueWithoutNotify(10.0f * mouseY);
     }
 
     public void ContinueButton()
@@ -40,5 +67,84 @@ public class MainMenu : MonoBehaviour
     {
         settingsPanel.SetActive(!settingsPanel.activeInHierarchy);
         mainPanel.SetActive(!mainPanel.activeInHierarchy);
+    }
+
+    public void ShowControlsPanel()
+    {
+        controlsPanel.SetActive(true);
+        audioPanel.SetActive(false);
+        graphicsPanel.SetActive(false);
+    }
+
+    public void ShowAudioPanel()
+    {
+        controlsPanel.SetActive(false);
+        audioPanel.SetActive(true);
+        graphicsPanel.SetActive(false);
+    }
+
+    public void ShowGraphicsPanel()
+    {
+        controlsPanel.SetActive(false);
+        audioPanel.SetActive(false);
+        graphicsPanel.SetActive(true);
+    }
+
+    public void SetMouseXSensitivity()
+    {
+        var value = horizontalSensitivitySlider.value;
+        var sens = (float)value / 10.0f;
+        PlayerPrefs.SetFloat(HORIZONTAL_SENS_KEY, sens);
+        horizontalSensitivityIF.SetTextWithoutNotify(value.ToString("F0"));
+    }
+
+    public void EnsureIntegerHorizontal(string value)
+    {
+        value = horizontalSensitivityIF.text;
+
+        if (int.TryParse(value, out var intVal)) return;
+
+        horizontalSensitivityIF.SetTextWithoutNotify("5");
+    }
+
+    public void SetMouseXSensitivityInputField(string s)
+    {
+        s = horizontalSensitivityIF.text;
+
+        if (int.TryParse(s, out var intVal))
+        {
+            var sens = intVal / 10.0f;
+            PlayerPrefs.SetFloat(HORIZONTAL_SENS_KEY, sens);
+            horizontalSensitivitySlider.SetValueWithoutNotify(intVal);
+        }
+    }
+
+    public void SetMouseYSensitivity()
+    {
+        var value = verticalSensitivitySlider.value;
+        var sens = (float)value / 10.0f;
+        PlayerPrefs.SetFloat(VERTICAL_SENS_KEY, sens);
+        verticalSensitivityIF.SetTextWithoutNotify(value.ToString("F0"));
+    }
+
+    public void EnsureIntegerVertical(string value)
+    {
+        value = verticalSensitivityIF.text;
+
+        if (int.TryParse(value, out var intVal)) return;
+
+        verticalSensitivityIF.SetTextWithoutNotify("5");
+    }
+
+    public void SetMouseYSensitivityInputField(string s)
+    {
+        s = verticalSensitivityIF.text;
+
+        if (int.TryParse(s, out var intVal))
+        {
+            var sens = intVal / 10.0f;
+            PlayerPrefs.SetFloat(VERTICAL_SENS_KEY, sens);
+            verticalSensitivitySlider.SetValueWithoutNotify(intVal);
+        }
     }
 }
