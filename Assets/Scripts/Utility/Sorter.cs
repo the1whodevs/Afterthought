c0ff395ce36ac84f7292a27121f4da33b8094f64
Gerasimos Filippakos <gerfil18-dev@yahoo.com>
2021-02-19 20:11:55 +0200

    Adjusted sorting

--- Assets/Scripts/Utility/Sorter.cs
+++ Assets/Scripts/Utility/Sorter.cs
@@ -23,6 +23,11 @@ public class Sorter : IComparer<WeaponData>, IComparer<EquipmentData>, IComparer
 
     public int Compare(TalentData x, TalentData y)
     {
+        if (x.isUnlocked && y.isUnlocked) return string.Compare(x.name, y.name, false, System.Globalization.CultureInfo.CurrentCulture);
+
+        if (x.isUnlocked) return -1;
+        if (y.isUnlocked) return 1;
+
         // If the talents unlock the same way...
         if (x.unlockType.Equals(y.unlockType))
         {
@@ -60,6 +65,11 @@ public class Sorter : IComparer<WeaponData>, IComparer<EquipmentData>, IComparer
 
     public int Compare(EquipmentData x, EquipmentData y)
     {
+        if (x.isUnlocked && y.isUnlocked) return string.Compare(x.name, y.name, false, System.Globalization.CultureInfo.CurrentCulture);
+
+        if (x.isUnlocked) return -1;
+        if (y.isUnlocked) return 1;
+
         // If the talents unlock the same way...
         if (x.unlockType.Equals(y.unlockType))
         {
@@ -97,6 +107,11 @@ public class Sorter : IComparer<WeaponData>, IComparer<EquipmentData>, IComparer
 
     public int Compare(WeaponData x, WeaponData y)
     {
+        if (x.isUnlocked && y.isUnlocked) return string.Compare(x.name, y.name, false, System.Globalization.CultureInfo.CurrentCulture);
+
+        if (x.isUnlocked) return -1;
+        if (y.isUnlocked) return 1;
+
         // If the talents unlock the same way...
         if (x.unlockType.Equals(y.unlockType))
         {
