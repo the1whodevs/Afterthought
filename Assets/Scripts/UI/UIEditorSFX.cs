c8b0474bc5da68e37c0c0b9e649481c98bb62f7e
Sneaky_Vaseline <13-12698@saeinsitute.edu>
2021-03-04 15:28:52 +0200

    LoadoutEditor SFX Almost Done

---
+++ Assets/Scripts/UI/UIEditorSFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEditorSFX : MonoBehaviour
+{
+    [Header("Buttons SFX")]
+    [SerializeField] private AudioSource UIAudioSource;
+    [SerializeField] private AudioClip hoverSFX;
+    [SerializeField] private AudioClip clickSFX;
+
+    private void Start()
+    {
+        UIAudioSource.GetComponent<AudioSource>();
+    }
+    public void PlayHoverSFX()
+    {
+        UIAudioSource.PlayOneShot(hoverSFX);
+    }
+
+    public void PlayClickSFX()
+    {
+        UIAudioSource.PlayOneShot(clickSFX);
+    }
+}
