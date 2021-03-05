e63733e6542fc4e2e15f90b30397729ff64c9934
Sneaky_Vaseline <13-12698@saeinsitute.edu>
2021-03-05 17:08:20 +0200

    Portal Ring Logic Kinda Works?

---
+++ Assets/Scripts/Interactables/PortalRingInteractable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalRingInteractable : InteractableObject
+{
+    [SerializeField, Min(0)] private int sceneBuildIndexDestination = 0;
+
+    public override string GetActionPronoun()
+    {
+        return "the";
+    }
+
+    public override string GetActionVerb()
+    {
+        return "travel to";
+    }
+
+    public override void Interact()
+    {
+        SceneManager.LoadScene(sceneBuildIndexDestination);
+    }
+}
