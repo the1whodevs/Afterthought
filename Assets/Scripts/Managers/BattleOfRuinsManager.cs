51bab5c2cf33f337df779104d02f40f97071bf75
Sneaky_Vaseline <13-12698@saeinsitute.edu>
2021-03-07 20:01:18 +0200

    Created Battle of Ruins Manager

---
+++ Assets/Scripts/Managers/BattleOfRuinsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleOfRuinsManager : MonoBehaviour
+{
+    [SerializeField] private LoadoutData battleOfRuinsData;
+
+    [Header("OnObjectiveComplete Actions")]
+    [SerializeField] private OnObjectiveCompleteWrapper[] onObjectiveCompleteActions;
+
+    [Header("First Defence")]
+    [SerializeField] private GameObject[] FirstDefenseTargets;
+
+    [Header("Second Defence")]
+    [SerializeField] private GameObject[] SecondDefenseTargets;
+
+    [Header("Stairs Defence")]
+    [SerializeField] private GameObject[] StairsDefenseTargets;
+
+    [Header("Last Defence")]
+    [SerializeField] private GameObject[] LastDefenseTargets;
+
+}
