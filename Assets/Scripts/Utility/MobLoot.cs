5b33e376a7d51962e8b61adf85a8652d492b79d2
Sneaky_Vaseline <13-12698@saeinsitute.edu>
2021-03-07 16:03:34 +0200

    Loot Logic + Health Loot Visibility

---
+++ Assets/Scripts/Utility/MobLoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobLoot : LootCube
+{
+    private EmeraldAI.EmeraldAISystem attachedSystem;
+    [SerializeField] private Collider lootCollider;
+
+    protected override void Start()
+    {
+        base.Start();
+        lootCollider.enabled = false;
+        attachedSystem = GetComponent<EmeraldAI.EmeraldAISystem>();
+        attachedSystem.DeathEvent.AddListener(() => { lootCollider.enabled = true; });
+    }
+
+    public override void Interact()
+    {
+        if (attachedSystem.CurrentHealth <= 0)
+        {
+            base.Interact();
+        }
+    }
+
+    public override void Loot()
+    {
+        IsLooted = true;
+    }
+}
