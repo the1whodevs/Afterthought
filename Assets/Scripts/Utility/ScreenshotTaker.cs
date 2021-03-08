c02710565a4a30405dd2639a1ccb38b676a9787f
Gerasimos Filippakos <gerfil18-dev@yahoo.com>
2021-03-08 04:46:35 +0200

    Screenshot Taker

---
+++ Assets/Scripts/Utility/ScreenshotTaker.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class ScreenshotTaker : MonoBehaviour
+{
+    [SerializeField] private KeyCode screenshotKey;
+
+    [SerializeField, Header("SCREENSHOT FILE NAME ONLY!")] 
+    private string screenshotPath = "SCREENSHOT";
+
+    private int screenshotIndex = 0;
+
+#if UNITY_EDITOR
+
+    private void Start()
+    {
+        screenshotPath = SaveSystem.SAVES_DIR + screenshotPath;
+
+        while (File.Exists($"{screenshotPath}_{screenshotIndex}{SaveSystem.SCREENSHOT_EXT}"))
+        {
+            screenshotIndex++;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(screenshotKey))
+        {
+            SaveSystem.TakeScreenshot($"{screenshotPath}_{screenshotIndex}{SaveSystem.SCREENSHOT_EXT}");
+            screenshotIndex++;
+        }
+    }
+
+#endif
+}
